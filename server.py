791c07333772b36c4c90c5a672724348b8215fae
statik2002 <statik2002@mail.ru>
2024-01-08 12:27:40 +0800

    add 404 exception and page

--- server.py
+++ server.py
@@ -1,47 +1,26 @@
 import asyncio
+import os.path
 from datetime import datetime
 from subprocess import Popen, PIPE
+
+import aiohttp.web
 from aiohttp import web
 import aiofiles
 
 INTERVAL_SECS = 1
 
 
-class Handler:
-
-    def __init__(self):
-        pass
-
-    async def handle_archive(self, request):
-        hash = request.match_info.get('archive', 'hz')
-
-        return web.Response()
-
-
-async def uptime_handler(request):
-    response = web.StreamResponse()
-
-    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
-    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
-    response.headers['Content-Type'] = 'text/html'
-
-    # Отправляет клиенту HTTP заголовки
-    await response.prepare(request)
-
-    while True:
-        formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
-
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-
-        await asyncio.sleep(INTERVAL_SECS)
-
-
 async def archive(request):
     hash = request.match_info.get('archive_hash', 'hz')
     args_set = ['-r', '-', '.']
-    proc = await asyncio.create_subprocess_exec('zip', *args_set, stdout=PIPE, stderr=PIPE, cwd=f'test_photos/{hash}/')
+
+    if not os.path.exists(f'test_photos/{hash}/'):
+        async with aiofiles.open('404.html', mode='r') as error_file:
+            error_contents = await error_file.read()
+        raise aiohttp.web.HTTPNotFound(body=error_contents, content_type='text/html')
+
+    proc = await asyncio.create_subprocess_exec(
+        'zip', *args_set, stdout=PIPE, stderr=PIPE, cwd=f'test_photos/{hash}/')
     response = web.StreamResponse()
     response.headers['Content-Type'] = 'text/html; charset=utf-8'
     response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
@@ -69,6 +48,5 @@ if __name__ == '__main__':
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archive),
-        web.get('/time/', uptime_handler)
     ])
     web.run_app(app)
